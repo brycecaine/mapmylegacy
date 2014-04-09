a3e7071fddb78a6fc01a8c622a5c6c69afa0fe01
Bryce Caine <caine@suu.edu>
2014-04-08 22:34:23 -0600

    Changed static-file variables to use the HOME environment variable

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -2,6 +2,8 @@
 # Django settings for OpenShift project.
 import imp, os
 
+HOME = os.environ['HOME']
+
 # a setting to determine whether we are running on OpenShift
 ON_OPENSHIFT = False
 if os.environ.has_key('OPENSHIFT_REPO_DIR'):
@@ -90,7 +92,7 @@ STATIC_URL = '/static/'
 if 'OPENSHIFT_REPO_DIR' in os.environ:
     STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
 else:
-    STATIC_ROOT = os.path.join('/home/action/workspace/mapmylegacy/wsgi', STATIC_URL.strip("/"))
+    STATIC_ROOT = os.path.join('%s/workspace/mapmylegacy/wsgi' % HOME, STATIC_URL.strip("/"))
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -107,7 +109,7 @@ if 'OPENSHIFT_REPO_DIR' in os.environ:
     )
 else:
     STATICFILES_DIRS = (
-        '/home/action/workspace/mapmylegacy/wsgi/openshift/static',
+        '%s/workspace/mapmylegacy/wsgi/openshift/static' % HOME,
     )
 
 # List of finder classes that know how to find static files in
