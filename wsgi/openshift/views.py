d8119089d80e7604b014484a33c9686be9d7a6cb
Bryce Caine <brycecaine@gmail.com>
2014-04-09 00:38:01 -0600

    Moved much of the views.py logic into service.py so it can be reused; worked on UI

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,65 +1,30 @@
 from django.shortcuts import render
-import json
-import os
-import requests
+import service
+import settings
 import urllib
 
-FS_CLIENT_ID = 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK'
-FS_AUTH_NETLOC = 'https://sandbox.familysearch.org'
-FS_AUTH_PATH = '/cis-web/oauth2/v3/authorization'
-FS_AUTH_PARAMS = \
-    {'redirect_uri': 'http://localhost:3000/select-person',
-     'response_type': 'code',
-     'client_id': FS_CLIENT_ID}
-
-FS_TOKEN_PATH = '/cis-web/oauth2/v3/token'
-FS_TOKEN_PARAMS = \
-    {'grant_type': 'authorization_code',
-     'client_id': FS_CLIENT_ID}
-
-FS_NETLOC = 'https://sandbox.familysearch.org'
+FS_AUTH_NETLOC = settings.FS_AUTH_NETLOC
+FS_AUTH_PATH = settings.FS_AUTH_PATH
+FS_AUTH_PARAMS = settings.FS_AUTH_PARAMS 
 
 def home(request):
-    fs_auth_params = urllib.urlencode(FS_AUTH_PARAMS)
-    fs_auth_url = '%s%s?%s' % (FS_AUTH_NETLOC,
-                               FS_AUTH_PATH,
-                               fs_auth_params)
+    curr_person_id = service.get_curr_person_id(request)
+    if curr_person_id:
+        next_url = '/select-person'
+        next_url_text = 'Select Ancestor'
+    else:
+        fs_auth_params = urllib.urlencode(FS_AUTH_PARAMS)
+        fs_auth_url = '%s%s?%s' % (FS_AUTH_NETLOC,
+                                   FS_AUTH_PATH,
+                                   fs_auth_params)
+        next_url = fs_auth_url
+        next_url_text = 'Sign in using FamilySearch'
 
     return render(request, 'home/home.html', locals())
 
 def select_person(request):
-    # -------------------------------------------------------------------------
-    # Get access token
-    fs_token_url = '%s%s' % (FS_AUTH_NETLOC, FS_TOKEN_PATH)
-    fs_auth_code = request.GET.get('code')
-    fs_token_params = FS_TOKEN_PARAMS
-    fs_token_params['code'] = fs_auth_code
-    fs_token_params_json = json.dumps(fs_token_params)
-    fs_token_headers = {'content-type': 'application/json'}
-    
-    r_auth = requests.post(fs_token_url, data=fs_token_params)
-    fs_access_token = r_auth.json()['access_token']
-
-    # -------------------------------------------------------------------------
-    # Get current person id
-    fs_person_path = '/platform/users/current.json'
-    fs_person_url = '%s%s' % (FS_NETLOC, fs_person_path)
-    fs_auth_headers = {'Authorization': 'Bearer %s' % fs_access_token}
-    r_person = requests.get(fs_person_url, headers=fs_auth_headers)
-    r_person_json = r_person.text
-    r_person_dict = json.loads(r_person_json)
-    curr_person_id = r_person_dict['users'][0]['personId']
-
-    # -------------------------------------------------------------------------
-    # Get ancestry data
-    fs_ancestry_path = '/platform/tree/ancestry.json'
-    fs_ancestry_url = '%s%s' % (FS_NETLOC, fs_ancestry_path)
-    fs_ancestry_params = {'person': curr_person_id}
-    fs_ancestry_params_json = json.dumps(fs_ancestry_params)
-    fs_auth_headers = {'Authorization': 'Bearer %s' % fs_access_token}
-    # r_ancestry = requests.get(fs_ancestry_url, data=fs_ancestry_params_json, headers=fs_auth_headers)
-    r_ancestry = requests.get('https://sandbox.familysearch.org/platform/tree/ancestry.json', params=fs_ancestry_params, headers=fs_auth_headers)
-    r_ancestry_json = r_ancestry.text
+    curr_person_id = service.get_curr_person_id(request)
+    r_ancestry_json = service.get_ancestry_data(request)
 
     return render(request, 'home/select-person.html', locals())
 
@@ -70,6 +35,10 @@ def map(request):
 def timeline(request):
 
     return render(request, 'home/timeline.html', locals())
+
+def logout(request):
+    # Delete the access token
+    return render(request, 'home/home.html', locals())
 
 def custom_404(request):
     return render(request, 'home/404.html')
